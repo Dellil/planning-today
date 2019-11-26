6b8c4c8ca650e0a821f44e8fa0243baa7d0afd3b
Dellil <aksaksdm@naver.com>
2019-11-26 16:37:10 +0900

    Added button event

--- src/component/content/ContentMainComponent.vue
+++ src/component/content/ContentMainComponent.vue
@@ -10,9 +10,12 @@
             >
                 <v-btn
                 block
-                text
+                :text="item.textState"
+                :color="item.color"
+
+                @click="btnClicked(item)"
                 >
-                    <v-icon v-text="mdiCheck"></v-icon>
+                    <v-icon v-text="item.icon" :color="item.iconColor"></v-icon>
                 </v-btn>
             </v-col>
             <v-col 
@@ -39,42 +42,81 @@
 </template>
 
 <script>
-import { mdiCheck, mdiDelete } from '@mdi/js';
+import { mdiCheck, mdiCheckOutline, mdiDelete } from '@mdi/js';
+import { isBuffer } from 'util';
 
 export default {
     data() {
         return {
-            length: 0,
             mdiCheck: mdiCheck,
+            mdiCheckOutline: mdiCheckOutline,
             mdiDelete: mdiDelete,
             // Mock Object
             items: [
                 {
+                    isChecked: false,
+                    textState: true,
+                    color: 'white',
+                    icon: mdiCheck,
+                    iconColor: "white",
                     text: '새벽기상',
                     order:1,
                     idx: 1
                 },
                 {
+                    isChecked: false,
+                    textState: true,
+                    color: 'white',
+                    icon: mdiCheck,
+                    iconColor: "white",
                     text: '알고리즘 문제 풀기',
                     order:1,
                     idx: 2
                 },
                 {
+                    isChecked: false,
+                    textState: true,
+                    color: 'white',
+                    icon: mdiCheck,
+                    iconColor: "white",
                     text: '독서',
                     order:2,
                     idx: 3
                 },
                 {
+                    isChecked: false,
+                    textState: true,
+                    color: 'white',
+                    icon: mdiCheck,
+                    iconColor: "white",
                     text: '집안일',
                     order:3,
                     idx: 4
                 },
                 {
+                    isChecked: false,
+                    textState: true,
+                    color: 'white',
+                    icon: mdiCheck,
+                    iconColor: "white",
                     text: '롸?',
                     order:4,
                     idx: 5
                 }
             ]
+        }
+    },
+    methods: {
+        btnClicked: function(item){
+            if(!item.isChecked){
+                item.isChecked = !item.isChecked;
+                item.textState = !item.textState;
+                item.color = 'grey darken-1';
+            }else{
+                item.isChecked = !item.isChecked;
+                item.textState = !item.textState;
+                item.color = 'white';
+            }
         }
     },
 }
